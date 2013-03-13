ed69ea949e1cfca36ca86465b8ec02f879cd119e
Joe Colosimo <jwc@jwcxz.com>
2013-03-13 03:21:45 -0400

    switch to a more traditional phase overflow method

--- sw/metronome.py
+++ sw/metronome.py
@@ -1,14 +1,12 @@
 import math, threading, time
 
 class Metronome(threading.Thread):
-    tpb = None
-    tpb_floor_affinity = None;
-    tpb_floor_choices = None;
     timestep = None;
     do_phaselock = True;
     tempo = None;
-    tick_counter = 0;
-    beat_counter = [0, 0];
+    tick_increment = None;
+    ticker = 0;
+    rx_beat_counter = 0;
     beatfn = None;
 
     enabled = False;
@@ -30,18 +28,18 @@ class Metronome(threading.Thread):
         if self.running:
             self.m_stop();
             self.tempo = newtempo;
-            self.calc_ticks_per_beat();
+            self.calc_tick_increment();
             self.m_start();
         else:
             self.tempo = newtempo;
-            self.calc_ticks_per_beat();
+            self.calc_tick_increment();
 
 
     def m_start(self):
         self.enabled = True;
         if not self.running:
-            self.tick_counter = 0;
-            self.beat_counter = [0, 0];
+            self.ticker = 0;
+            self.rx_beat_counter = 0;
             self.running = True;
 
 
@@ -54,43 +52,9 @@ class Metronome(threading.Thread):
         self.m_start();
 
 
-    def calc_ticks_per_beat(self):
-        # calculate ticks per beat with linear correction
-
-        # full floating point calculation
-        tpb_flt = 1/(self.timestep * (self.tempo * 1/60.));
-        self.tpb = (math.floor(tpb_flt), tpb_flt, math.ceil(tpb_flt));
-
-        # the ratio of the number of times choosing floor(tpb_flt) to the
-        # number of times choosing ceil(tpb_flt) should match
-        # tbp_flt-floor(tpb_flt)
-        self.tpb_floor_affinity = tpb_flt - math.floor(tpb_flt);
-
-        # reset actual floor-to-ceiling ratio 
-        self.tpb_floor_choices = [0, 1];
-
-
-    def get_ticks_per_beat(self, update_choices=True):
-        if not self.tpb:
-            self.calc_ticks_per_beat();
-
-        # TODO: just keep track of existing ratio and count... whatever, just
-        # an optimization
-        fcratio = float(self.tpb_floor_choices[0]) / \
-                  float(self.tpb_floor_choices[1]);
-
-        if fcratio <= self.tpb_floor_affinity:
-            # choose floor
-            if update_choices:
-                self.tpb_floor_choices[0] += 1;
-                self.tpb_floor_choices[1] += 1;
-            return self.tpb[0];
-        
-        else:
-            # choose ceiling
-            if update_choices:
-                self.tpb_floor_choices[1] += 1;
-            return self.tpb[1];
+    def calc_tick_increment(self):
+        # calculate the fraction of a beat that a tick represents
+        self.tick_increment = self.timestep * (self.tempo * 1/60.);
 
 
     def run(self):
@@ -103,49 +67,33 @@ class Metronome(threading.Thread):
                 time.sleep(0);
 
             while self.running:
-                if self.tick_counter >= self.get_ticks_per_beat():
-                    self.beat_counter[0] += 1;
-
-                    if self.beatfn: self.beatfn(self.beat_counter);
-                    #print "! %d" % self.beat_counter[0];
-
-                    self.tick_counter = 0;
-                else:
-                    self.tick_counter += 1;
+                _ = self.ticker;
+                self.ticker += self.tick_increment;
+                
+                if self.beatfn and int(_) < int(self.ticker):
+                    self.beatfn((self.ticker, self.rx_beat_counter));
 
                 time.sleep(self.timestep);
 
 
     def receive_beat(self, probability):
         # add to received beat counter
-        self.beat_counter[1] += 1;
+        self.rx_beat_counter += 1;
 
         # calculate beat offset
-        #i = float(self.beat_counter[0] - self.beat_counter[1])/float(self.beat_counter[1]);
-        i = float(self.beat_counter[0] - self.beat_counter[1]);
-        f = self.tick_counter / self.tpb[1];
-
-        if self.tick_counter >= self.tpb[1]/2:
-            f -= 1.0;
-            i -= 1;
+        beat_offset = self.ticker - self.rx_beat_counter;
 
         # adjust phase to be closer to received beat based on probability that
         # the received beat was in fact a beat
         if self.do_phaselock:
-            if self.tick_counter >= self.tpb[1]/2:
-                # move backward
-                self.tick_counter = (1-probability)*self.tick_counter + \
-                                    ( probability )*self.get_ticks_per_beat(False);
-            else:
-                # move forward
-                #self.tick_counter = 0*probability + self.tick_counter*(1-probability);
-                self.tick_counter *= (1 - probability);
+            self.ticker = (    probability ) * self.rx_beat_counter + \
+                          (1 - probability ) * self.ticker;
 
-        return i + f;
+        return beat_offset;
 
 
     def get_beat_count(self):
-        return self.beat_counter;
+        return (int(self.ticker), self.rx_beat_counter);
 
     def get_tempo(self):
         return self.tempo;
