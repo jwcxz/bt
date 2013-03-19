7c7d87bb61a1b865d3a65ce0210c93c301ea8c16
Joe Colosimo <jwc@jwcxz.com>
2013-03-19 19:11:30 -0400

    beat classifier and audio injector built

---
+++ sw/beatclassifier.py
@@ -0,0 +1,85 @@
+import threading, time
+
+class BeatClassifier(threading.Thread):
+    injfn = None;
+
+    samplerate = 44100; # reasonable default
+    timestep = None;
+    
+    bufsize = None;
+    samplebuf = [];
+
+    last_beat = 0;
+
+    cfg = {
+            'bufseconds': 2,
+            'avg_decay': .25,
+            'beat_thresh': 1.5
+        };
+
+
+    def __init__(self, injfn, *args, **kwargs):
+        threading.Thread.__init__(self);
+        for key in kwargs: self.cfg[key] = kwargs[key];
+        self.set_samplerate(self.samplerate);
+        self.injfn = injfn;
+
+
+    def set_samplerate(self, samplerate):
+        self.samplerate = samplerate;
+        self.timestep = 1/float(samplerate);
+        self.bufsize = self.__nsamples(self.cfg['bufseconds']);
+        blen = len(self.samplebuf);
+
+        if blen < self.bufsize:
+            self.samplebuf += [0]*(self.bufsize - blen);
+        elif blen > self.bufsize:
+            self.samplebuf = self.samplebuf[:self.bufsize];
+
+
+    def receive_sample(self, sample):
+        # TODO: for now, only consider one channel, even if it's stereo
+        self.samplebuf.append(sample[0]);
+
+
+    def run(self):
+        self.enabled = True;
+
+        while self.enabled:
+            # apply algorithm
+            self.classifier_round();
+            self.samplebuf.pop(0);
+            time.sleep(self.timestep);
+
+
+    def stop(self):
+        self.enabled = False;
+
+    
+    running_avg = 0;
+    def classifier_round(self):
+        # running average for baseline volume
+        self.running_avg = (1 - self.cfg['avg_decay'])*self.running_avg + \
+                            (self.cfg['avg_decay'])*((self.samplebuf[-1])**2);
+
+        # look for vu pulse shape lasting about 15ms
+        # highest tempo would be like 180bpm, so a beat every .3 sec
+        
+        # take sliding window of about 15ms and look for significantly higher
+        # average of absolute values than normal
+
+        self.last_beat += 1;
+
+        # only look for a beat if it's been over 75ms since the last beat
+        if self.last_beat >= self.__nsamples(0.075):
+            window_size = int(self.__nsamples(0.015));
+            window = self.samplebuf[-window_size:];
+            window_average = sum([s**2 for s in window])/float(window_size);
+
+            if window_average >= self.cfg['beat_thresh'] * self.running_avg:
+                self.injfn(1.0);
+                self.last_beat = 0;
+
+
+    def __nsamples(self, seconds):
+        return self.samplerate * seconds;
--- sw/bt.py
+++ sw/bt.py
@@ -2,9 +2,52 @@
 
 import sys
 
-import metronome, bank
+import metronome, bank, injector, beatclassifier
 
 if __name__ == "__main__":
+    mbank = bank.MetronomeBank(81, narrow_field=False);
+
+    def beat_inject(p):
+        print "-------"
+        mbank.inject_beat(p);
+        perrs = mbank.get_phase_errors();
+        min_p = None;
+        for p in perrs:
+            if min_p == None or abs(p[1]) < min_p[1]:
+                min_p = (p[0], abs(p[1]));
+            print " %3.3f: %f" %(p[0], p[1]);
+
+        print "BEST ESTIMATE: %3.5f [%f] => %d BPM" %(min_p[0], min_p[1], round(min_p[0]));
+        
+
+    bc = beatclassifier.BeatClassifier(beat_inject);
+    sinjector = injector.SampleInjector(sys.argv[1], bc.receive_sample);
+
+    print "Starting metronomes...";
+    mbank.start_metronomes();
+
+    print "Starting sample injector...";
+    sinjector.start();
+
+    print "Starting beat classifier...";
+    bc.start();
+
+    raw_input();
+
+    print "Stopping metronomes...";
+    mbank.stop_metronomes();
+
+    print "Stopping beat classifier...";
+    bc.stop();
+
+    print "Stopping injector...";
+    sinjector.stop();
+    sinjector.destroy();
+
+    print "Done!";
+
+
+    """
     target_tempo = float(sys.argv[1]);
 
     #mbank = bank.MetronomeBank(13, narrow_field=False);
@@ -35,3 +78,4 @@ if __name__ == "__main__":
     mbank.stop_metronomes();
 
     m_injector.stop();
+    """
