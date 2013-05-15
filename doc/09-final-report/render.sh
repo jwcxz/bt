0e25c7ea1881dec567b3939bc797c4d1368d7b47
Joe Colosimo <jwc@jwcxz.com>
2013-05-15 07:49:28 -0400

    started on final report

---
+++ doc/09-final-report/render.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+jobdir=$(realpath `dirname $0`)
+jobname=${jobdir##*/}
+
+cd "$jobdir"
+
+
+# convert figures with inkscape
+if [[ "$fig" != "" ]]; then
+    cd fig
+
+    for file in *.svg; do
+        echo "Processing $file..."
+        inkscape --without-gui --export-area-drawing --export-pdf="${file%svg}pdf" -f "$file"
+    done
+
+    cd ..
+fi
+
+export TEXINPUTS=.:./tex:../common/tex:$TEXINPUTS
+xelatex -jobname="$jobname" -halt-on-error -output-directory=out/ ./tex/main.tex
